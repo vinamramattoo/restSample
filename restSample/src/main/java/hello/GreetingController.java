21b9aebaeca26f52de3382078bf9bc112778e186
vinamramattoo <mattoo.vinamra@gmail.com>
2018-07-13 21:42:36 +0530

    added sample rest service

--- restSample/src/main/java/hello/GreetingController.java
+++ restSample/src/main/java/hello/GreetingController.java
@@ -11,6 +11,12 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @RequestMapping("/")
+    public Greeting test(@RequestParam(value="name", defaultValue="Naveen is noob") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
